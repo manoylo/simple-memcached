571a5829cb12abcdc12ce899814cd2bae86c481f
yuriy.manoylo <yuriy.m@rpplatform.com>
2016-09-23 14:54:11 +0300

    memcached

--- index.php
+++ index.php
@@ -6,3 +6,12 @@
  * Time: 2:48 PM
  */
 echo "Hello, simple";
+
+define('MEMCACHED_HOST', '134.168.59.218');
+define('MEMCACHED_PORT', '11211');
+$memcache = new Memcache;
+$cacheAvailable = $memcache->connect(MEMCACHED_HOST, MEMCACHED_PORT);
+
+echo "Memcached connected ? ";
+
+var_dump($cacheAvailable);
